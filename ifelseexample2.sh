310d0102aafee7f9e777d5f3398f1ba7c484b5cc
sahilgohil501 <sg2066037@gmail.com>
2025-01-16 11:49:20 +0530

    this is 2 example of if-else condition from java t pounr.

---
+++ ifelseexample2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# when condition is true
+# True && FALSE ||FALSE||TRUE
+if [[ 10 -gt 9 && 10 == 9 || 2 -lt 1 || 25 -gt 20 ]]; 
+then
+  echo "Given condition is true."
+  else
+   echo "Given condition is false."
+   fi
+   # When condition is false  
+#TRUE && FALSE || FALSE || TRUE  
+if [[ 10 -gt 9 && 10 == 8 || 3 -gt 4 || 8 -gt 8 ]]; 
+then
+echo "Given condition is true."
+else
+  echo "Given condition is not true"
+  fi
