52c11d137ba51fba20ff573aa083e683ee2c9432
sahilgohil501 <sg2066037@gmail.com>
2025-01-17 16:21:13 +0530

    This is my file as oops example i have show in chrome

---
+++ OOPSExample.py
@@ -0,0 +1,10 @@
+# OOP Example
+class Animal:
+    def __init__(self, name):
+        self.name = sahil
+    
+    def speak(self):
+        print(f"{self.name} makes a sound.")
+
+dog = Animal("Dog")
+dog.speak()
