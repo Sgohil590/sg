db80ddb2122ea8fccc1f4e8fb49b08375353b062
sahilgohil501 <sg2066037@gmail.com>
2025-01-18 22:57:28 +0530

    This is final script task assign by aaquib bhai also complete with testing in ubuntu server

---
+++ ubuntuuserscript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Define the username and password
+USERNAME="newuser"
+PASSWORD="password"
+
+# Create the new user
+useradd -m -s /bin/bash "$USERNAME"
+
+# Set the password for the new user
+echo "$USERNAME:$PASSWORD" | chpasswd
+
+# Add the new user to the sudo group
+usermod -aG sudo "$USERNAME"
+
+# Print a message indicating the user has been created
+echo "User $USERNAME created and added to the sudo group with root privileges."
